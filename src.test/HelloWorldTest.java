432f6ed64c3dbe72bb28a9a6566ee1ac771c0ae7
125642 <125642@DEREMWFH377PWF2.AMERICAS.GLOBAL.NTTDATA.COM>
2019-11-03 18:29:33 +0100

    Test

--- src.test/HelloWorldTest.java
+++ src.test/HelloWorldTest.java
@@ -4,11 +4,6 @@ import src.HelloWorld;
 
 public class HelloWorldTest extends TestCase {
 
-	protected void setUp() throws Exception {
-		super.setUp();
-	}
-	
-	
 	public void testMethod1() {
 		HelloWorld world = new HelloWorld();
 		assertEquals("TestPassed","hi",world.method1());
