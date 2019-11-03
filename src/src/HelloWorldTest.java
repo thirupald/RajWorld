5ec9c7d0cc9aad21ea17208ab08adccd75112f5d
125642 <125642@DEREMWFH377PWF2.AMERICAS.GLOBAL.NTTDATA.COM>
2019-11-03 19:11:52 +0100

    te

--- src/src/HelloWorldTest.java
+++ src/src/HelloWorldTest.java
@@ -5,6 +5,9 @@ import src.HelloWorld;
 
 public class HelloWorldTest extends TestCase {
 
+	public static void main (String[] args) {
+		System.out.println("Test");
+	}
 	public void testMethod1() {
 		HelloWorld world = new HelloWorld();
 		assertEquals("TestPassed","hi",world.method1());
